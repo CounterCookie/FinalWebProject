f25564ecd73f468a89ca06caaa92207a9f649543
Phong Pham <phong_pham88@hotmail.com>
2016-11-30 10:21:15 -0700

    adding table
    adding twit channels owners table

--- FinalProject/FinalProject-war/src/java/helpers/MainPageHelper.java
+++ FinalProject/FinalProject-war/src/java/helpers/MainPageHelper.java
@@ -108,12 +108,52 @@ public class MainPageHelper {
         } catch (SQLException ex) {
             Logger.getLogger(MainPageHelper.class.getName()).log(Level.SEVERE, null, ex);
         }
-        System.out.println(resulttable);
 
         return resulttable;
     }
     public String displayOwnedChanell(String user){
-        
-        return null;
+         String resulttable = "";
+        try {
+            resulttable += "<th>Channel Name</th><th>Username</th><th>Follow</th>";
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            InitialContext ic = new InitialContext();
+            DataSource ds = (DataSource) ic.lookup("jdbc/twitsdbPool");
+            Connection conn = ds.getConnection();
+
+            //Using Statement
+            String sql = "call channelGetMyChannels('"+user+"')";
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery(sql);
+            ResultSet rs1 = null;
+            Date date = null;
+            Time time = null;
+            while (rs.next()) {
+                resulttable += "<tr>";
+                resulttable += "<td>" + rs.getString(2) + "</td>";
+                String sql1 = "call channelLastTwitDateTime('" + rs.getInt(1) + "')";
+                rs1 = st.executeQuery(sql1);
+                if (rs1 != null && rs1.next()) {
+                    date = rs1.getDate(1);
+                    time = rs1.getTime(1);
+                }
+                resulttable += "<td>" + date + " " + time + "</td>";
+                resulttable += "<td>Delete</td>";
+                resulttable += "</tr>";
+                rs1.close();
+            }
+            st.close();
+            rs.close();
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(MainPageHelper.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (NamingException ex) {
+            Logger.getLogger(MainPageHelper.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            Logger.getLogger(MainPageHelper.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            Logger.getLogger(MainPageHelper.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(MainPageHelper.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return resulttable;
     }
 }
